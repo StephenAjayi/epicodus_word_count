f64e3dd7b030211eb31ffb703e8cdfae9e0f8596
Stephen <lonelytreesl@yahoo.com>
2015-04-24 20:44:17 +0000

    add second spec.

--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -5,4 +5,6 @@ describe('String#word_counter') do
   it("will allow user input string to be broken up into individual words") do
     expect("I am so very happy, happy, happy to be getting my code review today.".word_counter()).to(eq(["I", "am", "so", "very", "happy,", "happy,", "happy", "to", "be", "getting", "my", "code", "review", "today." ]))
   end
+  it("will remove punctuation in order to get an accurate word count") do 
+    expect("a,.".word_count()).to(eq["a"])
 end
