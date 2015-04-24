74174a314c9f1eaa938e50e27ae3f6a71b91f577
Stephen <lonelytreesl@yahoo.com>
2015-04-24 18:20:53 +0000

    initial files with first spec not passing.

---
+++ spec/word_counter_spec.rb
@@ -0,0 +1,8 @@
+require('rspec')
+require('word_counter')
+
+describe('String#word_counter') do 
+  it("will allow user input string to be broken up into individual words") do
+    expect("I am so very happy, happy, happy to be getting my code review today.".word_counter()).to(eq(["I", "am", "so", "very", "happy,", "happy," "happy", "to", "be", "getting", "my", "code", "review", "today." ]))
+  end
+end
